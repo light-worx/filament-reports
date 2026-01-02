dabdf75e7b7a9b35b2e5031208f1155ba22e7390
Michael Bishop <michael@bishop.net.za>
2026-01-02 13:17:34 +0200

    Add smart quotes method

--- src/Reports/BaseReport.php
+++ src/Reports/BaseReport.php
@@ -141,4 +141,18 @@ abstract class BaseReport extends tFPDF
         $this->AliasNbPages();
         return parent::Output($dest, $name, $isUTF8);
     }
+
+    public function convert_smart_quotes($string) {
+        $search = array(chr(0xe2) . chr(0x80) . chr(0x98),
+                        chr(0xe2) . chr(0x80) . chr(0x99),
+                        chr(0xe2) . chr(0x80) . chr(0x9c),
+                        chr(0xe2) . chr(0x80) . chr(0x9d),
+                        chr(0xe2) . chr(0x80) . chr(0x93),
+                        chr(0xe2) . chr(0x80) . chr(0x94),
+                        chr(226) . chr(128) . chr(153),
+                        'â€™','â€œ','â€<9d>','â€"','Â  ');
+        $replace = array("'","'",'"','"',' - ',' - ',"'","'",'"','"',' - ',' ');
+    
+        return str_replace($search, $replace, $string);
+    }
 }
